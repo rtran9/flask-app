1004da7740af21dbbc8c2b78ea1292199f7e5183
rtran9 <rtranare@gmail.com>
2015-09-21 04:24:49 -0400

    v1

---
+++ web.py
@@ -0,0 +1,15 @@
+__author__ = 'robert'
+
+from flask import Flask, jsonify, request, render_template
+import os
+
+app = Flask(__name__)
+app.debug = True
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
