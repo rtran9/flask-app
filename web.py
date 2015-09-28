d7a1af29910bd39d621ae9601b767c1e1ad15df3
rtran9 <rtranare@gmail.com>
2015-09-28 04:27:38 -0400

    website update

--- web.py
+++ web.py
@@ -7,8 +7,8 @@ app = Flask(__name__)
 app.debug = True
 
 @app.route('/')
-def home():
-    return render_template('home.html')
+def index():
+    return render_template('index.html')
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
